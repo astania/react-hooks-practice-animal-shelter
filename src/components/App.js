f7fb0cf56b78836815bcf8a736f2b8f4bfd2a30a
+ <+>
2022-02-11 13:49:29 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,47 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pets")
+      .then(r => r.json())
+      .then(allPets => setPets(allPets))
+
+  }, [])
+
+  const handlePetAdoption = (updatedPet) => {
+    const updatedPets = pets.map( pet => {
+      if(pet.id === updatedPet.id){
+        return updatedPet
+      } else {
+        return pet
+      }
+    })
+
+    setPets(updatedPets)
+  }
+
+  const handleChangeType = (event) => {
+    const selectedType = event.target.value
+    setFilters({type: selectedType})
+
+  }
+
+  const handleFindPets = () => {
+    if (filters.type === "all") {
+      fetch(`http://localhost:3001/pets`)
+      .then(r => r.json())
+      .then(selectedPets => setPets(selectedPets))
+    } else {
+      fetch(`http://localhost:3001/pets?type=${filters.type}`)
+      .then(r => r.json())
+      .then(selectedPets => setPets(selectedPets))
+    }
+  }
+
+
+
+
 
   return (
     <div className="ui container">
@@ -15,10 +56,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters filters={filters} setFilters={setFilters} onChangeType={handleChangeType} onFindPets={handleFindPets}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handlePetAdoption}/>
           </div>
         </div>
       </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,24 +1,44 @@
 import React from "react";
 
-function Pet() {
+
+
+function Pet({ pet, onAdoptPet }) {
+
+  // const [adoptPet, setAdoptPet] = useState(pet.isAdopted)
+
+  const handleAdoption = () => {
+    fetch(`http://localhost:3001/pets/${pet.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isAdopted: !pet.isAdopted,
+      }),
+    })
+      .then(r => r.json())
+      .then(updatedPet => onAdoptPet(updatedPet))
+  }
+
   return (
     <div className="card" data-testid="pet">
       <div className="content">
         <span className="header">
-          {/*'♀' OR '♂' */}
-          PET NAME
+          {pet.gender === "female" ? '♀' : '♂'}
+          {pet.name}
         </span>
         <div className="meta">
-          <span className="date">PET TYPE</span>
+          <span className="date">{pet.type}</span>
         </div>
         <div className="description">
-          <p>Age: PET AGE</p>
-          <p>Weight: PET WEIGHT</p>
+          <p>Age: {pet.age}</p>
+          <p>Weight: {pet.weight}</p>
         </div>
       </div>
       <div className="extra content">
-        <button className="ui disabled button">Already adopted</button>
-        <button className="ui primary button">Adopt pet</button>
+        {pet.isAdopted ? <button className="ui disabled button">Already Adopted</button> : <button onClick={handleAdoption} className="ui primary button">Adopt pet</button>}
+
+
       </div>
     </div>
   );
